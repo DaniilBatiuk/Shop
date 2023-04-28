c0b68c98813e2c7fb26fa895da0a598e73db6db6
DaniilBatiuk <sportak50@gmail.com>
2023-04-28 18:01:24 +0300

    create header menu

--- src/App.js
+++ src/App.js
@@ -1,47 +1,14 @@
 import React from "react";
 import "./styles/App.css";
-import gallery1 from './photos/gallery1.jpg';
-import gallery2 from './photos/gallery2.jpg';
-import gallery3 from './photos/gallery3.jpg';
-import gallery4 from './photos/gallery4.jpg';
-import gallery5 from './photos/gallery5.jpg';
+import Gallety from "./components/Gallery";
+import HeaderMenu from "./components/HeaderMenu";
+
 function App() {
   return (
     <>
-      <header className="header">
-        <nav className="header__menu">
-          <ul className="header__list">
-            <li>
-              <a href="" className="header__link">Categories</a>
-            </li>
-            <li>
-              <a href="" className="header__link">Product page</a>
-            </li>
-          </ul>
-        </nav>
-      </header>
+      <HeaderMenu></HeaderMenu>
       <main className="main">
-        <section className="main__gallery gallery">
-          <div className="gallery__container">
-            <div className="gallery__items">
-              <div className="gallery__item gallery__item_big">
-                <img src={gallery1} alt=""></img>
-              </div>
-              <div className="gallery__item gallery__item">
-                <img src={gallery2} alt=""></img>
-              </div>
-              <div className="gallery__item gallery__item">
-                <img src={gallery3} alt=""></img>
-              </div>
-              <div className="gallery__item gallery__item">
-                <img src={gallery4} alt=""></img>
-              </div>
-              <div className="gallery__item gallery__item">
-                <img src={gallery5} alt=""></img>
-              </div>
-            </div>
-          </div>
-        </section>
+        <Gallety></Gallety>
       </main>
       <footer className="footer">footer
       </footer>
---
+++ src/components/HeaderMenu.jsx
@@ -0,0 +1,42 @@
+import React from "react";
+import logo from '../photos/logo.jpg';
+function ShowMenu(e) {
+    const targetItem = e.target;
+    if (targetItem.closest('.icon-menu')) {
+        document.documentElement.classList.toggle('menu-open');
+    }
+}
+function HeaderMenu() {
+    return (
+        <header className="header">
+            <div className="header__container">
+                <div className="header__content">
+                    <a href="" className="header__logo">
+                        <img src={logo} alt="" />
+                    </a>
+                    <nav className="header__menu menu">
+                        <div className="menu__body">
+                            <ul className="menu__list">
+                                <li>
+                                    <a href="" className="menu__link">Categories</a>
+                                </li>
+                                <li>
+                                    <a href="" className="menu__link">Product page</a>
+                                </li>
+                            </ul>
+                        </div>
+                    </nav>
+                    <a href="" className="header__cart cart-header">
+                        <span className="cart-header__text">Cart</span>
+                        <span className="cart-header__quantity">0</span>
+                    </a>
+                    <button className="icon-menu" onClick={ShowMenu} type="button">
+                        <span></span>
+                    </button>
+                </div>
+            </div>
+        </header>
+    );
+}
+
+export default HeaderMenu;
