fc232fd8b00619534cd383a6b32c6857c175481e
DaniilBatiuk <sportak50@gmail.com>
2023-05-02 16:15:52 +0300

    Add photos and update Trending

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -1,15 +1,14 @@
 import React from "react";
-import chair1 from '../photos/chair1.jpg';
 
-function ProductCard() {
+function ProductCard({product}) {
     return (
         <div className="products-card__item">
             <div className="products-card__image">
-                <img src={chair1} alt="" />
+                <img src={product?.photo} alt="" />
             </div>
             <div className="products-card__body">
-                <div className="products-card__name">Chair M-45 Mustard corduroy</div>
-                <div className="products-card__price">455$</div>
+                <div className="products-card__name">{product?.title}</div>
+                <div className="products-card__price">{product?.price}$</div>
             </div>
         </div>
     );
--- src/components/Trending.jsx
+++ src/components/Trending.jsx
@@ -1,24 +1,57 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
 import ProductCard from "./ProductCard";
+import chair3 from '../photos/chair3.jpg';
+import chair4 from '../photos/chair4.jpg';
+import sofa1 from '../photos/sofa1.jpg';
+import sofa4 from '../photos/sofa4.jpg';
+import sofa5 from '../photos/sofa5.jpg';
+import nightstand1 from '../photos/nightstand1.jpg';
+import nightstand4 from '../photos/nightstand4.jpg';
+import nightstand5 from '../photos/nightstand5.jpg';
+import table1 from '../photos/table1.jpg';
+import table4 from '../photos/table4.jpg';
+
+const productList = [
+    { photo: chair3, title: "Semi-bar stool B-22 Copper-corduroy", price: 97.99 },
+    { photo: chair4, title: "Semi-bar chair B-140-1 Azure", price: 112.99 },
+    { photo: sofa1, title: "Sofa Luciano Gray", price: 988.99 },
+    { photo: sofa4, title: "Sofa Orlando Ice", price: 891.99 },
+    { photo: sofa5, title: "Sofa Dominic Gray", price: 399.99 },
+    { photo: nightstand1, title: "Bedside table Teneta S120 Black", price: 459.99 },
+    { photo: nightstand4, title: "Sideboard Bourbon 3 MDF Oak Nemo Latte", price: 170.99 },
+    { photo: nightstand5, title: "Picasso cabinet Blue Lagoon MDF", price: 105.99 },
+    { photo: table1, title: "Table HT-001 D100 White", price: 230.99 },
+    { photo: table4, title: "Corner table 120x80 Black", price: 225.99 }
+];
 
 function Trending() {
+    const scrollContainerRef = useRef(null);
+
+    useEffect(() => {
+        const scrollContainer = scrollContainerRef.current;
+
+        if (scrollContainer) {
+            scrollContainer.addEventListener("wheel", (e) => {
+                e.preventDefault();
+                scrollContainer.scrollLeft += e.deltaY;
+            });
+        }
+    }, []);
+
     return (
         <section className="trending">
             <div className="trending__container">
-                <div className="trending__title title">Trending Now</div>
-                <div className="trending__content">
-                    <div className="trending__items">
-                        <ProductCard></ProductCard>
-                        <ProductCard></ProductCard>
-                        <ProductCard></ProductCard>
-                        <ProductCard></ProductCard>
-                        <ProductCard></ProductCard>
-                        <ProductCard></ProductCard>
-                        <ProductCard></ProductCard>
-                        <ProductCard></ProductCard>
-                        <ProductCard></ProductCard>
-                        <ProductCard></ProductCard>
+                <div className="trending__title__buttons">
+                    <div className="trending__title title">Trending Now</div>
+                    <div className="trending__buttons">
+                        <button className="trending__button">&#8592;</button>
+                        <button className="trending__button">&#8594;</button>
                     </div>
+                </div>
+                <div className="trending__items" ref={scrollContainerRef}>
+                    {productList.map((elem) => (
+                        <ProductCard key={elem.title} product={elem} />
+                    ))}
                 </div>
             </div>
         </section>
